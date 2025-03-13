9b5757aa73be220e0256d51b60aba2f01de757c7
DanilaVV <voznyuk.danila@yandex.ru>
2025-03-13 05:31:39 +0000

    commit2

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 
+unsigned int	ft_strlen(char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
@@ -20,7 +30,10 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		j++;
 	}
 	dest[i] = '\0';
-	return (i);
+	if (size < l)
+		return (size + ft_strlen(src));
+	else
+		return (i - 1 + ft_strlen(src));
 }
 /*int	main(void)
 {
@@ -28,7 +41,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	char s2[10] = "Vozniuk";
 	int res;
 
-	res = strlcat(s1, s2, 9);
+	res = strlcat(s1, s2, 8);
 	puts(s1);
 	printf("%d", res);
 	return (0);
